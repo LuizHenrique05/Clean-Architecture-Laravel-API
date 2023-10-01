6009d76c30941f3138f85ec7420eff1e4bc2aa76
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2023-10-01 14:25:29 -0300

    refactoring: add jsonresponse as return type

--- app/Http/Controllers/Api/V1/Posts/StoreController.php
+++ app/Http/Controllers/Api/V1/Posts/StoreController.php
@@ -10,12 +10,12 @@ use App\Http\Requests\Api\V1\Posts\StoreRequest;
 use JustSteveKing\StatusCode\Http;
 use Domain\Blogging\Actions\CreatePost;
 use Domain\Blogging\Factories\PostFactory;
+use Illuminate\Http\JsonResponse;
 
 class StoreController extends Controller
 {
-    public function __invoke(StoreRequest $request)
+    public function __invoke(StoreRequest $request) : jsonResponse
     {
-
         $post = CreatePost::handle(
             object: PostFactory::create(
                 attributes: $request->validated()
