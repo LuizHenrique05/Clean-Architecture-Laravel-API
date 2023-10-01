76fcded040b5ca65198f62f60ce0e47ee2a5f7cd
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2023-10-01 10:48:35 -0300

    chore: creating posts by user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,13 +5,25 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
     public function run() : void
     {
-        $this->call(
-            class: DefaultUserSeeder::class,
-        );
+        // if(app()->environment('local')) {
+        //     $this->call(
+        //         class: DefaultUserSeeder::class,
+        //     );
+        // }
+
+        Post::factory(20)->for(
+            User::factory()->create([
+                'first_name' => 'Luiz',
+                'last_name' => 'Simba',
+                'email' => 'luiz.simba@gmail.com'
+            ])
+        )->create();
     }
 }
