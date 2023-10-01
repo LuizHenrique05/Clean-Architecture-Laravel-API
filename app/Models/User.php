ad6605513de719d2d914b52d0eea452b7f2e787f
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2023-10-01 09:37:00 -0300

    feat: setup laravel/jetstream/laravel-key-factory

---
+++ app/Models/User.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use JustSteveKing\KeyFactory\Models\Concerns\HasKeys;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasKeys;
+    use HasFactory;
+    use Notifiable;
+    use SoftDeletes;
+    use HasApiTokens;
+    use HasProfilePhoto;
+    use TwoFactorAuthenticatable;
+
+    protected $fillable = [
+        'key',
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'theme',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    protected $appends = [
+        'profile_photo_url',
+    ];
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('key')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('theme')->nullable();
+            $table->rememberToken();
+            $table->string('profile_photo_path', 2048)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
