c81bdc5a5d238fe60ff5aab41ce978a0da6014e8
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2023-10-01 15:08:30 -0300

    refactor: removing unused import

--- app/Http/Controllers/Api/V1/Posts/StoreController.php
+++ app/Http/Controllers/Api/V1/Posts/StoreController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Api\V1\Posts;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Http\Requests\Api\V1\Posts\StoreRequest;
 use JustSteveKing\StatusCode\Http;
 use Domain\Blogging\Actions\CreatePost;
