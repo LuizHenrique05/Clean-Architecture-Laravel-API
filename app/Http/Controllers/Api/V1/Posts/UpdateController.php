35c6a40d981f7b9eee419445a7e231d40730f271
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2023-10-01 15:37:47 -0300

    refactor: dispatch update post to job

--- app/Http/Controllers/Api/V1/Posts/UpdateController.php
+++ app/Http/Controllers/Api/V1/Posts/UpdateController.php
@@ -7,17 +7,17 @@ namespace App\Http\Controllers\Api\V1\Posts;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Response;
 use Domain\Blogging\Models\Post;
-use Domain\Blogging\Actions\UpdatePost;
 use Domain\Blogging\Factories\PostFactory;
 use App\Http\Requests\Api\V1\Posts\UpdateRequest;
 use JustSteveKing\StatusCode\Http;
+use App\Jobs\Posts\UpdatePost;
 
 class UpdateController extends Controller
 {
     public function __invoke(UpdateRequest $request, Post $post) : Response
     {
-        UpdatePost::handle(
-            object: PostFactory::createToUpdatePost(
+        UpdatePost::dispatch(
+            PostFactory::createToUpdatePost(
                 attributes: $request->validated(),
                 post: $post
             ),
---
+++ app/Jobs/Posts/UpdatePost.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Jobs\Posts;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Domain\Blogging\Actions\UpdatePost as UpdatePostAction;
+use Domain\Blogging\ValueObjects\PostValueObject;
+use Domain\Blogging\Models\Post;
+
+class UpdatePost implements ShouldQueue
+{
+    use Queueable;
+    use Dispatchable;
+    use SerializesModels;
+    use InteractsWithQueue;
+
+    public function __construct(
+        public PostValueObject $object,
+        public Post $post
+    ) {}
+
+    public function handle() : void
+    {
+        UpdatePostAction::handle(
+            object: $this->object,
+            post: $this->post
+        );
+    }
+}
