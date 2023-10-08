67955a76742f96eb4d909a5ca5931afe481e67c1
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2023-10-08 15:01:37 -0300

    feat: fetch a post

--- app/Http/Controllers/Api/V1/Posts/ShowController.php
+++ app/Http/Controllers/Api/V1/Posts/ShowController.php
@@ -6,11 +6,28 @@ namespace App\Http\Controllers\Api\V1\Posts;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Domain\Blogging\Models\Post;
+use Illuminate\Http\Response;
+use App\Http\Resources\Api\V1\PostResource;
+use Illuminate\Http\JsonResponse;
+use Spatie\QueryBuilder\QueryBuilder;
+use JustSteveKing\StatusCode\Http;
 
 class ShowController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, Post $post) : jsonResponse
     {
+        $post = QueryBuilder::for(
+            subject: Post::class,
+        )->allowedIncludes(
+            includes: ['user']
+        )->first();
 
+        return response()->json(
+            data: new PostResource(
+                resource: $post,
+            ),
+            status: Http::OK
+        );
     }
 }
