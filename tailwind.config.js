7c40f31a4515d0ac8a0fee6333af43e457152c14
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2023-10-08 16:09:19 -0300

    feat: using tailwind to create form

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    mode: 'jit',
+    darkMode: 'media',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
